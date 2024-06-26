691749f7f79b2f2925b9e584b34ec8ae45bbc728
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-06-27 00:47:59 +0200

    fixed spinner again

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,5 +1,5 @@
 //import { Spinner } from "react-bootstrap";
-import spinnerGif from "../assets/images/spinner.gif";
+import spinnerGif from "../assets/images/pan.gif";
 
 const LoadingSpinner = () => {
   return (
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -4,6 +4,7 @@ import "aos/dist/aos.css";
 import { useDispatch, useSelector } from "react-redux";
 import { startLoading, stopLoading } from "../redux/actions/index.js";
 import LoadingSpinner from "./LoadingSpinner";
+import RegisterForm from "./RegisterForm.jsx";
 
 const RegisterPage = () => {
   const dispatch = useDispatch();
@@ -33,7 +34,15 @@ const RegisterPage = () => {
         <LoadingSpinner />
       ) : (
         <div>
-          <h2>Register Page</h2>
+          <div>
+            <h2 className="recipeTitle mt-4 mb-3">Join The Plant Based Hub</h2>
+            <p className="px-5 my-3 recipeBody">
+              Welcome to <strong>The Plant Based Hub</strong>! By creating an account, you will gain access to a wealth
+              of plant-based recipes, nutritional information, and a supportive community of like-minded individuals.
+              Join us in promoting healthy, sustainable, and compassionate eating.
+            </p>
+          </div>
+          <RegisterForm />
         </div>
       )}
     </div>
