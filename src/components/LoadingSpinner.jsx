9e2596167b75444eb000c24eb391417f2b7cf85b
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-06-26 23:34:25 +0200

    fixed spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,13 @@
-import { Spinner } from "react-bootstrap";
+//import { Spinner } from "react-bootstrap";
+import spinnerGif from "../assets/images/spinner.gif";
 
 const LoadingSpinner = () => {
   return (
-    <div className="loadingSpinnerContainer">
-      <Spinner className="loadingSpinner" animation="grow" />
+    // <div className="loadingSpinnerContainer">
+    //   <Spinner className="loadingSpinner" animation="grow" />
+    // </div>
+    <div className="spinner-container">
+      <img src={spinnerGif} alt="Loading..." className="spinner-image" />
     </div>
   );
 };
