af5abfe248365643a49d2defa9d6e3544c0f71c0
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-06-29 13:06:47 +0200

    added fav page

--- src/components/FavouritesPage.jsx
+++ src/components/FavouritesPage.jsx
@@ -1,5 +1,20 @@
+import { Container } from "react-bootstrap";
+import FavouritesTable from "./FavouritesTable";
+
 const FavouritesPage = () => {
-  return <div>fav page</div>;
+  return (
+    <Container className="text-center">
+      <div>
+        <h2 className="recipeTitle mt-4 mb-3">MY FAVOURITE RECIPES</h2>
+        <p className="px-5 mt-4 recipeBody">
+          Welcome to your Favorites section! Here you will find all the recipes you have saved and want to keep handy.
+          Whether you are looking for ideas for breakfast, lunch, dinner, or a snack, this page allows you to easily
+          access your favorite dishes and rediscover the recipes you love the most.
+        </p>
+      </div>
+      <FavouritesTable />
+    </Container>
+  );
 };
 
 export default FavouritesPage;
---
+++ src/components/FavouritesTable.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Table, Image, Button } from "react-bootstrap";
+import unavailable from "../assets/images/unavailable-recipe.png";
+import { BsBookmarkStarFill, BsTrash3Fill } from "react-icons/bs";
+
+const FavouritesTable = () => {
+  const recipes = [
+    {
+      id: 1,
+      image: "",
+      name: "Vegan Pancakes",
+      category: "Breakfast",
+    },
+    {
+      id: 2,
+      image: "",
+      name: "Quinoa Salad",
+      category: "Lunch",
+    },
+    {
+      id: 3,
+      image: "",
+      name: "Tofu Stir Fry",
+      category: "Dinner",
+    },
+  ];
+
+  return (
+    <Table className="favTable" responsive="sm">
+      <thead className="tHead">
+        <tr>
+          <th></th>
+          <th>Recipe Name</th>
+          <th>Recipe Category</th>
+          <th>Rating</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {recipes.map(recipe => (
+          <tr key={recipe.id} className="tableRow">
+            <td>
+              <Image
+                src={recipe.image || unavailable}
+                alt={recipe.name}
+                className="rounded-circle recipeImg"
+                width={100}
+                height={100}
+              />
+            </td>
+            <td>{recipe.name}</td>
+            <td>{recipe.category}</td>
+            <td></td>
+            <td>
+              <Button className="ratingBtn me-3">
+                <BsBookmarkStarFill />
+              </Button>
+              <Button className="deleteBtn">
+                <BsTrash3Fill />
+              </Button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default FavouritesTable;
